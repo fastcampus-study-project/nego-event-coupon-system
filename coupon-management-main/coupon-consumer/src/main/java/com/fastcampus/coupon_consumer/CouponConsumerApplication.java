7f3d96cc2e23fca0258c0954cf29891f42c6cd6c
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2025-05-15 20:28:34 +0900

    init : 프로젝트 생성
    - 쿠폰 api 모듈 프로젝트 생성
    - 쿠폰 consumer 모듈 프로젝트 생성
    - 쿠폰 core 모듈 프로젝트 생성

---
+++ coupon-management-main/coupon-consumer/src/main/java/com/fastcampus/coupon_consumer/CouponConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.coupon_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponConsumerApplication.class, args);
+	}
+
+}
---
+++ coupon-management-main/coupon-consumer/src/test/java/com/fastcampus/coupon_consumer/CouponConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.coupon_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
